20095aabc25d1d4caf5045fcc532f7a59e04ba7f
joankirui <joankirui99@gmail.com>
2021-11-27 23:21:45 +0300

    include image_location function

--- gallery/views.py
+++ gallery/views.py
@@ -10,4 +10,9 @@ def index(request):
     locations = Location.get_location()
 
     return render(request, 'index.html', {"title":title,"all_images":all_images,"locations":locations})
+
+def image_location(request,location):
+    images = Image.filter_by_location(location)
+    
+    return render(request, 'locations.html', {"images":images})
     
