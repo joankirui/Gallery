236d0f1a25332672a5243c6b14ff75d5d2ae7663
joankirui <joankirui99@gmail.com>
2021-11-27 12:59:50 +0300

    create first template

--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to my gallery')
+    return render(request,'welcome.html')
 
-def index(request):
+# def index(request):
     
