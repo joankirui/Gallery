2af4a3b86bc16f1ac90c73d9bbac162528f3d887
joankirui <joankirui99@gmail.com>
2021-11-27 16:16:06 +0300

    create second model Category

--- gallery/models.py
+++ gallery/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     location = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
