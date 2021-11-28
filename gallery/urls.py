9c5929a458718d6b7071706a54e6c0327f8e4aee
joankirui <joankirui99@gmail.com>
2021-11-28 15:56:51 +0300

    create urlpattern that references search_results

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
     
-    url(r'^$',views.index,name = 'index')
+    url(r'^$',views.index,name = 'index'),
+    url(r'^search/',views.search_results, name='search_results')
     
 ]
